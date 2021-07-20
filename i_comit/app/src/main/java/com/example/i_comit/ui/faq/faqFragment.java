7caa709cf2eebef42f3a36252e52b220f3019239
ezra1905 <72178147+ezra1905@users.noreply.github.com>
2021-07-20 20:17:29 +0700

    commit

---
+++ i_comit/app/src/main/java/com/example/i_comit/ui/faq/faqFragment.java
@@ -0,0 +1,19 @@
+package com.example.i_comit.ui.faq;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.i_comit.R;
+
+
+public class faqFragment extends Fragment {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_faq, container, false);
+    }
+}
